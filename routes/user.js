4d4fe96563990b17e131a3ba08c085b51532e1bb
CodeZilla740 <34anha38@gmail.com>
2026-07-16 18:57:03 +0530

    Sign up & log in

--- routes/user.js
+++ routes/user.js
@@ -13,8 +13,11 @@ router.post("/signup", wrapAsync(async (req, res) => {
         let { username, email, password } = req.body;
         const newUser=new User({ username, email });
         const registeredUser = await User.register(newUser, password);
-        req.flash('success', 'Welcome to Xtreme Living!');
-        res.redirect('/listings');
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Xtreme Living!');
+            res.redirect('/listings');
+        });
     }
     catch (e) {
         req.flash('error', e.message);
